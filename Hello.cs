1ed67a6a26cd0f79d0852717c35476fe8bc08ca2
NikWoss <nikwoss.info@gmail.com>
2025-09-26 15:00:25 +0400

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -7,6 +7,46 @@ namespace Problem
 {
     public class Hello
     {
-        
+        List<IAnimal> animals = new List<IAnimal> { new Dog() };
+        public void Run()
+        {
+            foreach (var animal in animals)
+            {
+                AnimalService test = new AnimalService(animal);
+                test.ShowSkills();
+            }
+        }
+
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("The University");
+    }
+    public interface IAnimal
+    {
+        void Move();
+        void Voice();
+    }
+    public class AnimalService
+    {
+        private readonly IAnimal _animal;
+        public AnimalService(IAnimal animal)
+        {
+            _animal = animal;
+        }
+        public void ShowSkills()
+        {
+            _animal.Move();
+            _animal.Voice();
+        }
+    }
+    public class Dog : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Dog is running..");
+        }
+        public void Voice()
+        {
+            Console.WriteLine("Hau!!!");
+        }
     }
 }
